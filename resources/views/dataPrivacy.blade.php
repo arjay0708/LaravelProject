7fa7aaf1c6e192efdae124f2f1ed087a06aa8ce3
GingpisJohnPaul <139692910+jieanmarcestudillo-dev@users.noreply.github.com>
2024-01-04 06:38:42 +0800

    change titel

--- resources/views/dataPrivacy.blade.php
+++ resources/views/dataPrivacy.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="{{ URL('/img/icon.png') }}" type="image/x-icon">
-    <title>Harbor View</title>
+    <title>HOSS</title>
     @include('cdn')
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/home/superfish.css') }}">
